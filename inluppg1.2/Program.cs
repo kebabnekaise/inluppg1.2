e78f01722654365157815c968f1ab097d482a6c1
emsi06001 <emsi06001@E-PW0234N3>
2023-09-06 12:39:32 +0200

    Add project files.

---
+++ inluppg1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace inluppg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sätt bakgrundsfärgen till blått. Färgen anges av ConsoleColor
+            Console.BackgroundColor = ConsoleColor.Blue;
+            //Gör så att hela bakgrunden tar färgen blå
+            Console.Clear();
+
+            //Skriv ut texten. Här används \n för att skapa en ny rad
+            Console.WriteLine(":( \nDatorn verkar ha kraschat buhu");
+
+            Console.ReadKey();
+        }
+    }
+}
